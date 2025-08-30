13e1df866fba20911e6d86364a7ae5f4fbd7202e
robertDurst <me@robdurst.com>
2025-08-29 21:55:38 -0600

    chore: update license from GPL-3.0 to AGPL-3.0-or-later

--- info.rkt
+++ info.rkt
@@ -6,4 +6,4 @@
 (define pkg-desc "XDR encoding, decoding, and generation library")
 (define version "0.0.1")
 (define pkg-authors '("Rob Durst"))
-(define license 'GPL-3.0)
+(define license 'AGPL-3.0-or-later)
