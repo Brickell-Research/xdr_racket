a3ca722ccdba50a392f42bca95cde3292aa27096
robertDurst <me@robdurst.com>
2025-08-29 22:06:29 -0600

    feat: add XDR unsigned integer encoding and decoding support

--- main.rkt
+++ main.rkt
@@ -1,14 +1,16 @@
 #lang typed/racket
 
 (provide
- ;; XDR Integer
+ ;; XDR Signed Integer
  xdr-int xdr-int? xdr-int-number xdr-decode-int xdr-encode-int
+ ;; XDR Unsigned Integer
+ xdr-uint xdr-uint? xdr-uint-number xdr-decode-uint xdr-encode-uint
  ;; XDR Boolean
  xdr-boolean xdr-boolean? xdr-boolean-value xdr-decode-boolean xdr-encode-boolean
  )
 
 
-;; XDR Integer
+;; XDR Signed Integer
 (struct xdr-int ([number : Integer]) #:transparent)
 
 (: xdr-decode-int (-> Bytes xdr-int))
@@ -18,6 +20,17 @@
 (: xdr-encode-int (-> xdr-int Bytes))
 (define (xdr-encode-int xdr-int)
   (integer->integer-bytes (xdr-int-number xdr-int) 4 #t #t))
+
+;; XDR Unsigned Integer
+(struct xdr-uint ([number : Integer]) #:transparent)
+
+(: xdr-decode-uint (-> Bytes xdr-uint))
+(define (xdr-decode-uint bstr)
+  (xdr-uint (integer-bytes->integer bstr #f #t)))
+
+(: xdr-encode-uint (-> xdr-uint Bytes))
+(define (xdr-encode-uint xdr-uint)
+  (integer->integer-bytes (xdr-uint-number xdr-uint) 4 #f #t))
 
 ;; XDR Boolean
 (struct xdr-boolean ([value : Boolean]) #:transparent)
--- test.rkt
+++ test.rkt
@@ -19,6 +19,16 @@
 (define MAX_INT_BYTES #"\x7f\xff\xff\xff")
 (define MIN_INT_BYTES #"\x80\x00\x00\x00")
 
+;; Unsigned Integer Constants as XDR Integers
+(define ZERO_UNSIGNED_XDR_INT (xdr-uint 0))
+(define ONE_UNSIGNED_XDR_INT (xdr-uint 1))
+(define MAX_UNSIGNED_XDR_INT (xdr-uint 4294967295))
+
+;; Unsigned Integer Constants as Bytes
+(define ZERO_UNSIGNED_INT_BYTES #"\x00\x00\x00\x00")
+(define ONE_UNSIGNED_INT_BYTES #"\x00\x00\x00\x01")
+(define MAX_UNSIGNED_INT_BYTES #"\xff\xff\xff\xff")
+
 ;; Boolean Constants as XDR Booleans
 (define FALSE_XDR_BOOLEAN (xdr-boolean #f))
 (define TRUE_XDR_BOOLEAN (xdr-boolean #t))
@@ -38,6 +48,14 @@
                (check-equal? MAX_XDR_INT (xdr-decode-int MAX_INT_BYTES))
                ;; min negative
                (check-equal? MIN_XDR_INT (xdr-decode-int MIN_INT_BYTES)))
+   (test-suite "Unsigned Integer Decoding Tests"
+               ;; 32-bit unsigned integer
+               ;; base
+               (check-equal? ZERO_UNSIGNED_XDR_INT (xdr-decode-uint ZERO_UNSIGNED_INT_BYTES))
+               ;; positive
+               (check-equal? ONE_UNSIGNED_XDR_INT (xdr-decode-uint ONE_UNSIGNED_INT_BYTES))
+               ;; max positive
+               (check-equal? MAX_UNSIGNED_XDR_INT (xdr-decode-uint MAX_UNSIGNED_INT_BYTES)))
    (test-suite "Boolean Decoding Tests"
                ;; base
                (check-equal? FALSE_XDR_BOOLEAN (xdr-decode-boolean ZERO_INT_BYTES))
@@ -59,6 +77,14 @@
                (check-equal? MAX_INT_BYTES (xdr-encode-int MAX_XDR_INT))
                ;; min negative
                (check-equal? MIN_INT_BYTES (xdr-encode-int MIN_XDR_INT)))
+   (test-suite "Unsigned Integer Encoding Tests"
+               ;; 32-bit unsigned integer
+               ;; base
+               (check-equal? ZERO_UNSIGNED_INT_BYTES (xdr-encode-uint ZERO_UNSIGNED_XDR_INT))
+               ;; positive
+               (check-equal? ONE_UNSIGNED_INT_BYTES (xdr-encode-uint ONE_UNSIGNED_XDR_INT))
+               ;; max positive
+               (check-equal? MAX_UNSIGNED_INT_BYTES (xdr-encode-uint MAX_UNSIGNED_XDR_INT)))
    (test-suite "Boolean Encoding Tests"
                ;; base
                (check-equal? ZERO_INT_BYTES (xdr-encode-boolean FALSE_XDR_BOOLEAN))
