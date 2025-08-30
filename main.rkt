212aee8aee2edc6f93442502c931470c2d30fd85
robertDurst <me@robdurst.com>
2025-08-29 22:17:22 -0600

    feat: add XDR hyper integer (64-bit) encoding and decoding support

--- main.rkt
+++ main.rkt
@@ -5,6 +5,10 @@
  xdr-int xdr-int? xdr-int-number xdr-decode-int xdr-encode-int
  ;; XDR Unsigned Integer
  xdr-uint xdr-uint? xdr-uint-number xdr-decode-uint xdr-encode-uint
+ ;; XDR Hyper Integer
+ xdr-hyper xdr-hyper? xdr-hyper-number xdr-decode-hyper xdr-encode-hyper
+ ;; XDR Unsigned Hyper Integer
+ xdr-uhyper xdr-uhyper? xdr-uhyper-number xdr-decode-uhyper xdr-encode-uhyper
  ;; XDR Boolean
  xdr-boolean xdr-boolean? xdr-boolean-value xdr-decode-boolean xdr-encode-boolean
  )
@@ -31,6 +35,28 @@
 (: xdr-encode-uint (-> xdr-uint Bytes))
 (define (xdr-encode-uint xdr-uint)
   (integer->integer-bytes (xdr-uint-number xdr-uint) 4 #f #t))
+
+;; XDR Hyper Integer (64-bit signed)
+(struct xdr-hyper ([number : Integer]) #:transparent)
+
+(: xdr-decode-hyper (-> Bytes xdr-hyper))
+(define (xdr-decode-hyper bstr)
+  (xdr-hyper (integer-bytes->integer bstr #t #t)))
+
+(: xdr-encode-hyper (-> xdr-hyper Bytes))
+(define (xdr-encode-hyper xdr-hyper)
+  (integer->integer-bytes (xdr-hyper-number xdr-hyper) 8 #t #t))
+
+;; XDR Unsigned Hyper Integer (64-bit unsigned)
+(struct xdr-uhyper ([number : Integer]) #:transparent)
+
+(: xdr-decode-uhyper (-> Bytes xdr-uhyper))
+(define (xdr-decode-uhyper bstr)
+  (xdr-uhyper (integer-bytes->integer bstr #f #t)))
+
+(: xdr-encode-uhyper (-> xdr-uhyper Bytes))
+(define (xdr-encode-uhyper xdr-uhyper)
+  (integer->integer-bytes (xdr-uhyper-number xdr-uhyper) 8 #f #t))
 
 ;; XDR Boolean
 (struct xdr-boolean ([value : Boolean]) #:transparent)
--- test.rkt
+++ test.rkt
@@ -29,6 +29,30 @@
 (define ONE_UNSIGNED_INT_BYTES #"\x00\x00\x00\x01")
 (define MAX_UNSIGNED_INT_BYTES #"\xff\xff\xff\xff")
 
+;; Hyper Integer Constants as XDR Hyper Integers
+(define ZERO_XDR_HYPER (xdr-hyper 0))
+(define ONE_XDR_HYPER (xdr-hyper 1))
+(define NEGATIVE_ONE_XDR_HYPER (xdr-hyper -1))
+(define MAX_XDR_HYPER (xdr-hyper 9223372036854775807))
+(define MIN_XDR_HYPER (xdr-hyper -9223372036854775808))
+
+;; Hyper Integer Constants as Bytes
+(define ZERO_HYPER_BYTES #"\x00\x00\x00\x00\x00\x00\x00\x00")
+(define ONE_HYPER_BYTES #"\x00\x00\x00\x00\x00\x00\x00\x01")
+(define NEGATIVE_ONE_HYPER_BYTES #"\xff\xff\xff\xff\xff\xff\xff\xff")
+(define MAX_HYPER_BYTES #"\x7f\xff\xff\xff\xff\xff\xff\xff")
+(define MIN_HYPER_BYTES #"\x80\x00\x00\x00\x00\x00\x00\x00")
+
+;; Unsigned Hyper Integer Constants as XDR Unsigned Hyper Integers
+(define ZERO_UNSIGNED_XDR_HYPER (xdr-uhyper 0))
+(define ONE_UNSIGNED_XDR_HYPER (xdr-uhyper 1))
+(define MAX_UNSIGNED_XDR_HYPER (xdr-uhyper 18446744073709551615))
+
+;; Unsigned Hyper Integer Constants as Bytes
+(define ZERO_UNSIGNED_HYPER_BYTES #"\x00\x00\x00\x00\x00\x00\x00\x00")
+(define ONE_UNSIGNED_HYPER_BYTES #"\x00\x00\x00\x00\x00\x00\x00\x01")
+(define MAX_UNSIGNED_HYPER_BYTES #"\xff\xff\xff\xff\xff\xff\xff\xff")
+
 ;; Boolean Constants as XDR Booleans
 (define FALSE_XDR_BOOLEAN (xdr-boolean #f))
 (define TRUE_XDR_BOOLEAN (xdr-boolean #t))
@@ -56,6 +80,26 @@
                (check-equal? ONE_UNSIGNED_XDR_INT (xdr-decode-uint ONE_UNSIGNED_INT_BYTES))
                ;; max positive
                (check-equal? MAX_UNSIGNED_XDR_INT (xdr-decode-uint MAX_UNSIGNED_INT_BYTES)))
+   (test-suite "Hyper Integer Decoding Tests"
+               ;; 64-bit signed hyper integer
+               ;; base
+               (check-equal? ZERO_XDR_HYPER (xdr-decode-hyper ZERO_HYPER_BYTES))
+               ;; positive
+               (check-equal? ONE_XDR_HYPER (xdr-decode-hyper ONE_HYPER_BYTES))
+               ;; negative
+               (check-equal? NEGATIVE_ONE_XDR_HYPER (xdr-decode-hyper NEGATIVE_ONE_HYPER_BYTES))
+               ;; max positive
+               (check-equal? MAX_XDR_HYPER (xdr-decode-hyper MAX_HYPER_BYTES))
+               ;; min negative
+               (check-equal? MIN_XDR_HYPER (xdr-decode-hyper MIN_HYPER_BYTES)))
+   (test-suite "Unsigned Hyper Integer Decoding Tests"
+               ;; 64-bit unsigned hyper integer
+               ;; base
+               (check-equal? ZERO_UNSIGNED_XDR_HYPER (xdr-decode-uhyper ZERO_UNSIGNED_HYPER_BYTES))
+               ;; positive
+               (check-equal? ONE_UNSIGNED_XDR_HYPER (xdr-decode-uhyper ONE_UNSIGNED_HYPER_BYTES))
+               ;; max positive
+               (check-equal? MAX_UNSIGNED_XDR_HYPER (xdr-decode-uhyper MAX_UNSIGNED_HYPER_BYTES)))
    (test-suite "Boolean Decoding Tests"
                ;; base
                (check-equal? FALSE_XDR_BOOLEAN (xdr-decode-boolean ZERO_INT_BYTES))
@@ -85,6 +129,26 @@
                (check-equal? ONE_UNSIGNED_INT_BYTES (xdr-encode-uint ONE_UNSIGNED_XDR_INT))
                ;; max positive
                (check-equal? MAX_UNSIGNED_INT_BYTES (xdr-encode-uint MAX_UNSIGNED_XDR_INT)))
+   (test-suite "Hyper Integer Encoding Tests"
+               ;; 64-bit signed hyper integer
+               ;; base
+               (check-equal? ZERO_HYPER_BYTES (xdr-encode-hyper ZERO_XDR_HYPER))
+               ;; positive
+               (check-equal? ONE_HYPER_BYTES (xdr-encode-hyper ONE_XDR_HYPER))
+               ;; negative
+               (check-equal? NEGATIVE_ONE_HYPER_BYTES (xdr-encode-hyper NEGATIVE_ONE_XDR_HYPER))
+               ;; max positive
+               (check-equal? MAX_HYPER_BYTES (xdr-encode-hyper MAX_XDR_HYPER))
+               ;; min negative
+               (check-equal? MIN_HYPER_BYTES (xdr-encode-hyper MIN_XDR_HYPER)))
+   (test-suite "Unsigned Hyper Integer Encoding Tests"
+               ;; 64-bit unsigned hyper integer
+               ;; base
+               (check-equal? ZERO_UNSIGNED_HYPER_BYTES (xdr-encode-uhyper ZERO_UNSIGNED_XDR_HYPER))
+               ;; positive
+               (check-equal? ONE_UNSIGNED_HYPER_BYTES (xdr-encode-uhyper ONE_UNSIGNED_XDR_HYPER))
+               ;; max positive
+               (check-equal? MAX_UNSIGNED_HYPER_BYTES (xdr-encode-uhyper MAX_UNSIGNED_XDR_HYPER)))
    (test-suite "Boolean Encoding Tests"
                ;; base
                (check-equal? ZERO_INT_BYTES (xdr-encode-boolean FALSE_XDR_BOOLEAN))
