5f54b9334134b4e6c556e1475bc670f0fd9e523b
robertDurst <me@robdurst.com>
2025-08-29 23:56:14 -0600

    chore: move rackunit-lib to build-deps and add compile-omit-paths for tests

--- info.rkt
+++ info.rkt
@@ -1,12 +1,13 @@
 #lang info
 
 (define collection "xdr")
-(define deps '("base" "typed-racket-lib" "rackunit-lib"))
+(define deps '("base" "typed-racket-lib"))
 (define build-deps
   '("racket-doc"
     "scribble-lib"
     "rackunit-lib"
     "rackunit-typed"))
+(define compile-omit-paths '("test.rkt"))
 (define pkg-desc "XDR encoding, decoding, and generation library")
 (define version "0.0.1")
 (define pkg-authors '("Rob Durst"))
