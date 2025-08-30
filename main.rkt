05b008be40d2dd46b7e5bbc8c2af8e0dbdd5e59b
robertDurst <me@robdurst.com>
2025-08-30 16:01:04 -0600

    fix imports for package

---
+++ main.rkt
@@ -0,0 +1,9 @@
+#lang typed/racket
+
+(require "xdr/integer.rkt"
+         "xdr/boolean.rkt"
+         "xdr/hyper.rkt"
+         "xdr/string.rkt"
+         "xdr/opaque.rkt")
+
+(provide (all-defined-out))
