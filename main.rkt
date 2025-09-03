84ebbbabf58752ecf933c02845ebe1c6891d5a0b
Rob Durst <16689974+robertDurst@users.noreply.github.com>
2025-09-03 13:42:31 -0600

    init cli (#14)

--- main.rkt
+++ main.rkt
@@ -1,21 +1,18 @@
 #lang typed/racket
 
-(require "xdr/types/integer.rkt"
-         "xdr/types/boolean.rkt"
-         "xdr/types/hyper.rkt"
-         "xdr/types/floating-point.rkt"
-         "xdr/types/string.rkt"
-         "xdr/types/opaque.rkt"
-         "xdr/types/void.rkt"
-         "xdr/types/array.rkt"
-         "xdr/types/enum.rkt")
-
-(provide (all-from-out "xdr/types/integer.rkt")
-         (all-from-out "xdr/types/boolean.rkt")
-         (all-from-out "xdr/types/hyper.rkt")
-         (all-from-out "xdr/types/floating-point.rkt")
-         (all-from-out "xdr/types/string.rkt")
-         (all-from-out "xdr/types/opaque.rkt")
-         (all-from-out "xdr/types/void.rkt")
-         (all-from-out "xdr/types/array.rkt")
-         (all-from-out "xdr/types/enum.rkt"))
+(module+ main
+  (require racket/cmdline)
+  
+  (command-line
+   #:program "xdr-racket"
+   #:usage-help
+   "XDR encoding, decoding, and generation library"
+   ""
+   "A Racket library for XDR (External Data Representation) encoding and decoding."
+   #:once-each
+   [("--version") "Show version information"
+    (displayln (format "xdr-racket version 0.0.1"))
+    (exit 0)]
+   #:args ()
+   (displayln "XDR Racket library")
+   (displayln "Use --help for available options")))
